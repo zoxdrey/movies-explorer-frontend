bed04d62f433bf0d2970123c1bc58ab3852cdbc9
v.petrov <zoxypva@gmail.com>
2021-11-06 14:12:45 +0300

    about me block

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
-import { AboutMe } from "../AboutMe/AboutMe";
+import AboutMe from "../AboutMe/AboutMe";
 import { Portfolio } from "../Portfolio/Portfolio";
 import Header from "../Header/Header";
 import "./Main.css";

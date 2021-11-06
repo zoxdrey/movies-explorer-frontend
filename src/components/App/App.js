8d244cacc89c7e4f2bc9c32c78d9fc2eeddafd08
v.petrov <zoxypva@gmail.com>
2021-11-06 15:54:25 +0300

    not found page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Main from "../Main/Main";
+import NotFound from "../NotFound/NotFound";
 
 function App() {
   return (
     <div className="app">
-      <Main />
+      <NotFound />
     </div>
   );
 }

267954b3eaefce6f8549f20ea15d00449ad52c71
v.petrov <zoxypva@gmail.com>
2021-11-14 20:13:33 +0300

    register login forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,34 @@
 import "./Login.css";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ email, password });
+  }
+
   return (
     <div className="login">
       <Link to={"/"} className="login__logo">
         <img src={logo} alt="логотип" />
       </Link>
 
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2 className="login-form__title">Рады видеть!</h2>
-        <label for="email" className="login-form__label">
+        <label htmlFor="email" className="login-form__label">
           E-mail
         </label>
         <input
@@ -19,8 +37,10 @@ const Login = () => {
           className="login-form__input"
           name="email"
           required
+          value={email}
+          onChange={handleEmailChange}
         ></input>
-        <label for="password" className="login-form__label">
+        <label htmlFor="password" className="login-form__label">
           Пароль
         </label>
         <input
@@ -30,6 +50,8 @@ const Login = () => {
           id="password"
           name="password"
           required
+          value={password}
+          onChange={handlePasswordChange}
         ></input>
         <button aria-label="Login" className="login-form__button" type="submit">
           Вход
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,17 +1,39 @@
 import "./Register.css";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
+import { register } from "../../utils/MainApi";
+import { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ name, email, password });
+  }
   return (
     <div className="register">
       <Link to={"/"} className="register__logo">
         <img src={logo} alt="логотип" />
       </Link>
 
-      <form className="register-form">
+      <form className="register-form" onSubmit={handleSubmit}>
         <h2 className="register-form__title">Добро пожаловать!</h2>
-        <label for="name" className="register-form__label">
+        <label htmlFor="name" className="register-form__label">
           Имя
         </label>
         <input
@@ -20,8 +42,10 @@ const Register = () => {
           className="register-form__input"
           name="name"
           required
+          value={name}
+          onChange={handleNameChange}
         ></input>
-        <label for="email" className="register-form__label">
+        <label htmlFor="email" className="register-form__label">
           E-mail
         </label>
         <input
@@ -30,8 +54,10 @@ const Register = () => {
           className="register-form__input"
           name="email"
           required
+          value={email}
+          onChange={handleEmailChange}
         ></input>
-        <label for="password" className="register-form__label">
+        <label htmlFor="password" className="register-form__label">
           Пароль
         </label>
         <input
@@ -41,6 +67,8 @@ const Register = () => {
           id="password"
           name="password"
           required
+          value={password}
+          onChange={handlePasswordChange}
         ></input>
         <button
           aria-label="register"

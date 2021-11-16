4151d07ac493e8e99dacd8fd3268664cc5669ae7
v.petrov <zoxypva@gmail.com>
2021-11-16 22:46:18 +0300

    add redirect deom protected routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,20 +9,42 @@ import Register from "../Register/Register";
 import { Route, Routes } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import { useState } from "react";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 function App() {
   const [currentUser, setCurrentUser] = useState();
-
+  const [loggedIn, setLoggedIn] = useState(true);
   return (
     <div className="app">
       <CurrentUserContext.Provider value={currentUser}>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/saved-movies" element={<SavedMovies />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="*" element={<NotFound />} />
+          <Route
+            path="/movies"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <Movies />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />{" "}
+          <Route
+            path="/saved-movies"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <SavedMovies />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </CurrentUserContext.Provider>
     </div>
--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Redirect, Route } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...props }) => {
-  return (
-    <Route>
-      {() =>
-        props.loggedIn ? <Component {...props} /> : <Redirect to="./signin" />
-      }
-    </Route>
-  );
+const ProtectedRoute = ({ children, ...props }) => {
+  let location = useLocation();
+
+  if (!props.loggedIn) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="../signin" state={{ from: location }} />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;

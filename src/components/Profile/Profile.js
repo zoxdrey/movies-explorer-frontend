069051edf36e7f42d8df4eb4560edbc6bf6e18a7
v.petrov <zoxypva@gmail.com>
2021-11-17 00:46:51 +0300

    add context to profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,24 @@
 import Header from "../Header/Header";
+import { useContext } from "react";
 import "./Profile.css";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 const Profile = () => {
+  const currentUser = JSON.parse(useContext(CurrentUserContext));
+
   return (
     <div className="profile__container">
       <Header showAuthNav={false} />
       <div className="profile">
         <div className="profile__info">
-          <div className="profile__title">Привет, Виталий!</div>
+          <div className="profile__title">Привет, {currentUser.name}!</div>
           <div className="profile__row">
             <div className="profile__text">Имя</div>
-            <div className="profile__text">Виталий</div>
+            <div className="profile__text">{currentUser.name}</div>
           </div>
           <div className="profile__devider"></div>
           <div className="profile__row">
             <div className="profile__text">E-mail</div>
-            <div className="profile__text">pochta@yandex.ru</div>
+            <div className="profile__text">{currentUser.email}</div>
           </div>
         </div>
         <div className="profile__bottom">

7b530496a03d6c2284b1e80fdb6bbfde305f2f70
v.petrov <zoxypva@gmail.com>
2021-11-20 19:06:34 +0300

    redirect from notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import "./NotFound.css";
-
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="not-found">
       <div className="not-found__text">
@@ -8,7 +10,14 @@ const NotFound = () => {
         <p className="not-found__subtitle">Страница не найдена</p>
       </div>
 
-      <div className="not-found__link">Назад</div>
+      <div
+        className="not-found__link"
+        onClick={() => {
+          navigate(-2);
+        }}
+      >
+        Назад
+      </div>
     </div>
   );
 };
